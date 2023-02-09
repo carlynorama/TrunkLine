b276ea91a0a766de9f09d3f1aa9f9c99a73e4620
carlynorama <carlynorama@gmail.com>
2023-02-09 15:30:48 -0800

    refactor auth code

--- Sources/TrunkLine/Mastodon/MastodonServer.swift
+++ Sources/TrunkLine/Mastodon/MastodonServer.swift
@@ -22,11 +22,11 @@ enum MastodonAPIError: Error, CustomStringConvertible {
 
 //apiBase: "/api/v1"
 public struct MastodonServer:APIServer, Authorizable {
-
     
     public private(set)var scheme: Scheme
     public private(set) var host: URL
     public private(set) var apiversion = APIVersion()
+    public private(set) var authentication: Authentication?
     
     
     public var version: String? {
@@ -40,58 +40,35 @@ public struct MastodonServer:APIServer, Authorizable {
     public init(host:URL, scheme:Scheme = .https) {
         self.host =  host
         self.scheme = scheme
-        self._token = readTokenFromKeyChain(accountKey:"tipsyrobot")
+        self.authentication = nil
     }
     
-    //AUTHORIZABLE
-    //TODO: Property Wrapper for token?
-    //TODO: /accounts/update_credentials
-     
-    public var token: String? {
-        _token
+    public var hasValidToken: Bool {
+        //not exactly true.
+        authentication != nil
     }
     
-    private var _token:String?
-    
-    mutating public func setToken(token: String) {
-        //TODO: confirm with server
-        _token = token
+    //STEPONE - does the keychain still work. 
+    public mutating func authorize(account:String, service:String, keyBase:String = Authentication.defaultKeyBase) throws {
+        do {
+            self.authentication =  try Authentication.makeFromKeyChain(account: account, service: service, keyBase: keyBase)
+        }
+        catch {
+            throw MastodonAPIError("Unable to authorize: \(error.localizedDescription)")
+        }
+        
     }
     
-    mutating public func clearToken() {
-        _token = nil
-    }
-    
-    public var isAuthorized: Bool {
-        _token != nil
-    }
-    
-    public var serviceKey: String = "access-token"
-    public var accountKeyBase: String = "TrunkLineLib"
-    
-    public func checkCredential(token:String) async throws -> Data {
+    public func checkCredential(authentication:Authentication) async throws -> Data {
         let path = apiversion.endpointPaths["verify"]
         let url = try? urlFrom(path: path!, usingAPIBase: true)
-        let header = appendAuthHeader(to:[:], token:token)
+        let header = try authentication.appendAuthHeader(to:[:])
         //TODO: BAD - I'm pointing at a RequestService implmentation.
         let request =  HTTPRequestService.buildRequest(for: url!, with: header)!
         let (data, _) = try await URLSession.shared.data(for: request)
         return data
     }
     
-    //"tipsyrobot"
-    mutating public func loadTokenFromKeyChain(for account:String) {
-        let readToken = readTokenFromKeyChain(accountKey: account)
-        _token = readToken // if setter saves to keychain can't user setter here.
-        //TODO: should verify be part of the loading?
-//        do {
-//            let data = try await checkCredential(token: accessTokenOut)
-//            print(String(data: data, encoding: .utf8)!)
-//        } catch {
-//            print(error)
-//        }
-    }
-    
 }
-    
-    
+
+
