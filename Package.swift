7d9adefdf0bcb5fae90eae4026dda1b2ebadec8e
carlynorama <carlynorama@gmail.com>
2023-02-09 10:05:03 -0800

    Update Package.swift
    point to repo

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        //.package(url: "https://github.com/carlynorama/APItizer.git", branch: "main"),
-        .package(path: "../APItizer")
+        .package(url: "https://github.com/carlynorama/APItizer.git", branch: "main"),
+        //.package(path: "../APItizer")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
