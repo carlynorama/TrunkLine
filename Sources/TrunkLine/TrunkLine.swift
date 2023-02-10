4a7cac915a310d4248602e55ff7b831504e456d7
carlynorama <carlynorama@gmail.com>
2023-02-09 18:11:26 -0800

    add loadDotEnv() convenience func

--- Sources/TrunkLine/TrunkLine.swift
+++ Sources/TrunkLine/TrunkLine.swift
@@ -18,6 +18,10 @@ public struct TrunkLine {
         
     }
     
+    public static func loadDotEnv() throws {
+       try DotEnv.loadDotEnv()
+    }
+    
     public static func fetchAuthFromEnvironment(account:String, service:String, tokenKey:String, secretsFile:URL? = nil) throws -> Authentication {
         do {
             let new = try Authentication.makeFromEnvironment(accountName: account, service: service, tokenKey: tokenKey)
